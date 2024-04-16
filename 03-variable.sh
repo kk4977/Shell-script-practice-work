c6227d0b91fce7dce8d4b2fd466e41587526074e
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-04-16 08:19:50 +0530

    changes by sivakumar

---
+++ 03-variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PERSON1=RAHUL
+PERSON2=Raju
+
+echo "$PERSON1 : Hello $PERSON2 , hOW ARE YUO?"
+echo "$PERSON2 : Hello $PERSON1, iAM fINE"
---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter the Username:"
+
+read USERNAME
+
+echo "Enter the password:"
+
+read PASSWORD
+
+echo " Username: $USERNAME PASS: $PASSWORD"
