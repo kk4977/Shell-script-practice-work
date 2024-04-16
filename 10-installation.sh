169ecc39ea6f49ae1e241d9f35e50e59333b4792
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-04-16 17:00:30 +0530

    10- installation.sh modified  by sivakumar

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+sudo apt get update 
+
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root"
+    exit 1
 else
-    echo "You are super user."
+    echo "You are a super user"
 fi
 
+# Install MySQL Server
+echo "Installing MySQL Server..."
+ 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of mysql...FAILURE"
-    exit 1
+# Check the exit status of the MySQL Server installation
+if [ $? -eq 0 ]; then
+    echo "Installation of MySQL Server succeeded"
 else
-    echo "Installation of mysql...SUCCESS"
+    echo "Installation of MySQL Server failed"
+    exit 1
 fi
 
+# Install Git
+echo "Installing Git..."
 dnf install git -y
 
-if [ $? -ne 0 ]
-then
-    echo "Installation of git...FAILURE"
-    exit 1
+# Check the exit status of the Git installation
+if [ $? -eq 0 ]; then
+    echo "Installation of Git succeeded"
 else
-    echo "Installation of Git...SUCCESS"
+    echo "Installation of Git failed"
+    exit 1
 fi
-
-echo "is script proceeding?"
