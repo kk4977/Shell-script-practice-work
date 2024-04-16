ce10c56600e578dcff944b24bb014d7610dae072
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-04-16 17:58:42 +0530

    10- installation.sh modified  by sivakumar

--- 10-installation.sh
+++ 10-installation.sh
@@ -11,7 +11,7 @@ else
     echo "You are a super user"
 fi
 
- 
+# Install MySQL Server
 echo "Installing MySQL Server..."
  
 dnf install mysql -y
@@ -22,11 +22,12 @@ else
     echo "Installation of MySQL Server failed"
     exit 1
 fi
- 
+
+# Install Git
 echo "Installing Git..."
 dnf install git -y
 
- 
+# Check the exit status of the Git installation
 if [ $? -eq 0 ]; then
     echo "Installation of Git succeeded"
 else
