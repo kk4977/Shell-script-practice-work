216edc16595a47234bc2ccfb84775ab31b70de82
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-04-18 18:23:02 +0530

    Updated by sivakumar

--- 11-function.sh
+++ 11-function.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(data +F%-H%-M%-%s)
+TIMESTAMP=$(date +F%-H%-M%-%s)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/temp/$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
-VALIDATE(){
-    if ($1 -ne 0) {
-        then 
+VALIDATE()
+{
+    if [$1 -ne 0] 
+    then 
         echo "$2...Failure"
     else
         echo "$2...SUCCESS"
+    
+    fi
 }
 
 
-if [ $USERID -ne 0 ]; then
-    echo "Please run this script as root"
-    exit 1
-else
-    echo "You are a super user"
-fi
+ 
 # Install MySQL Server
  
 dnf install mysql -y &>>$LOGFILE
@@ -32,9 +30,4 @@ dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git..."
 
 # Check the exit status of the Git installation
-if [ $? -eq 0 ]; then
-    echo "Installation of Git succeeded"
-else
-    echo "Installation of Git failed"
-    exit 1
-fi
+ 
