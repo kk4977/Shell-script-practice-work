cf1eba520387692f59e46bd9b9d766c1bdee67ab
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-04-24 10:42:13 +0530

    Code written and Updated and modified  by sivakumar

---
+++ 13-current-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COURSE="Devops from current Script"
+
+echo "Before calling other scripts, course : $COURSE"
+
+echo " Process ID of the  current shell script   : $$"
+
+#./14-other-scripts.sh
+
+source ./14-other-scripts.sh
+
+echo "After calling other script , course :  $COURSE"
+
+echo "Process ID of the other script : $$"
+
