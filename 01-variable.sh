25b8e1487d3dcc6f5a0b6353e7362af9e3d089c2
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2024-05-19 12:12:48 +0530

    Code Updated  by sivakumar

--- 01-variable.sh
+++ 01-variable.sh
@@ -3,5 +3,5 @@
 PERSON1=RAHUL
 PERSON2=Raju
 
-echo "$PERSON1 : Hello $PERSON2 , hOW ARE YUO?"
-echo "$PERSON2 : Hello $PERSON1, iAM fINE"
+echo "$PERSON1 : Hello $PERSON2 , How are you?"
+echo "$PERSON2 : Hello $PERSON1, I am Fine"
